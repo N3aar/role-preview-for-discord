98e962c6582678d2a77d5f283d402130b1beaa4a
Gabriel Tavares <bieltmd@outlook.com>
2025-06-20 15:33:34 -0400

    chore: put base url in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/role-preview-for-discord/",
   plugins: [react()],
   resolve: {
     alias: {
